7b0a0467f56639ebe9bb1aee568a06b5f40a4f3d
HXY-0227 <1473706220@qq.com>
2020-06-15 22:31:51 +0800

    斐波那契数列的非递归解法

--- leetcode/Solution509.py
+++ leetcode/Solution509.py
@@ -1,8 +1,26 @@
 class Solution:
+    """
+    递归算法实现
+    """
+    def fib1(self, N: int) -> int:
+        if N <= 1:
+            return N
+        return self.fib1(N - 1) + self.fib1(N - 2)
+    
+    """
+    通过数组的方式去实现
+    """
     def fib(self, N: int) -> int:
         if N <= 1:
             return N
-        return self.fib(N - 1) + self.fib(N - 2)
+        return self.memoize(N)
+        
+    
+    def memoize(self, N: int) -> {}:
+        cache = {0:0, 1:1}
+        for i in range(2, N + 1):
+            cache[i] = cache[i - 1] + cache[i - 2]
+        return cache[N]
 
 if __name__ == "__main__":
     solution = Solution()
